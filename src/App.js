6259084e3d53f8342714440616e5e42f084ef154
Christianbenjaminsson <54481384+Christianbenjaminsson@users.noreply.github.com>
2020-02-02 11:56:25 +0100

    Installed Router-dom and wirked with path to hats

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
 import './App.css';
 
-class App extends React.Component {
-  render() {
-    return (
-      <div classname='App'>
-        <HomePage />
-      </div>
-    );
-  }
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+);
+
+function App() {
+  return (
+    <div>
+      <Switch>
+        <Route exact path='/' component={HomePage}/>
+        <Route path='/hats' component={HatsPage}/>
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
