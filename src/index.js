8905f2f9b81f4b1a673ed5e43382754ca67cb6e3
Christianbenjaminsson <54481384+Christianbenjaminsson@users.noreply.github.com>
2020-02-25 18:27:17 +0100

    Install and start to set up Redux

--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux';
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <Provider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>,
+  </Provider>,
   document.getElementById('root')
   );
 
