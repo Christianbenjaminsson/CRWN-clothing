1f253eeecc8125c99679c7510dd7d7b74d0e121c
Christianbenjaminsson <54481384+Christianbenjaminsson@users.noreply.github.com>
2020-03-05 19:07:31 +0100

    Fix so shopping bag is displayed in header

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { ReactComponent as Logo } from '../../assets/crown.svg';
-import './header.styles.scss';
-import { auth } from '../../firebase/firebase.utils';
 import { connect } from 'react-redux';
-import cartIcon from '../cart-icon/cart-icon.component';
+import { auth } from '../../firebase/firebase.utils';
+import CartIcon from '../cart-icon/cart-icon.component';
 import CartDropdown from '../cart-dropdown/cart-dropdown.component';
+
+import { ReactComponent as Logo } from '../../assets/crown.svg';
+
+import './header.styles.scss';
 
 const Header = ({ currentUser, hidden }) => (
   <div className='header'>
@@ -28,9 +30,9 @@ const Header = ({ currentUser, hidden }) => (
           SIGN IN
         </Link>
       )}
-      <cartIcon />
+      <CartIcon />
     </div>
-    <CartDropdown /> 
+    {hidden ? null : <CartDropdown />}
   </div>
 );
 
