892572540f47256fac2af748123c3e85df37e1f8
Christianbenjaminsson <54481384+Christianbenjaminsson@users.noreply.github.com>
2020-04-21 08:01:47 +0200

    Rename category folder and files to collection

---
+++ src/pages/collection/collection.component.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './collection.styles.scss';
+import CollectionItem from '../../components/collection-item/collection-item.component';
+
+const CollectionPage = ({ match }) => {
+  console.log(match.params.collectionId);
+  return (
+    <div className='collection-page'>
+      <h2>COLLECTION PAGE</h2>
+    </div>
+  );
+};
+
+export default CollectionPage;
