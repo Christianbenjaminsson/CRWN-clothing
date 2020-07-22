7a15bf4ea8cbb07df32c9c9eaf7fb4233fd60095
Christianbenjaminsson <54481384+Christianbenjaminsson@users.noreply.github.com>
2020-07-22 13:21:58 +0200

    Remove method that has been written twice

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -51,21 +51,6 @@ export const addCollectionAndDocuments = async (collectionKey, objectsToAdd) => 
   return await batch.commit();
 };
 
-const convertCollectionsSnapshotToMap = (collections) => {
-  const transformedCollection = collections.docs.map(doc => {
-    const { title, items } = doc.data();
-
-    return {
-      routeName: encodeURI(title.toLowerCase()),
-      id: doc.id,
-      title,
-      items
-    };
-  });
-
-  console.log(transformedCollections);
-}
-
 export const convertCollectionsSnapshotToMap = collections => {
   const transformedCollection = collections.docs.map(doc => {
     const { title, items } = doc.data();
