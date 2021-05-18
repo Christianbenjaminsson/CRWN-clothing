556f5d73328e41cbac4faa6b873f17b85039fa48
Christianbenjaminsson <54481384+Christianbenjaminsson@users.noreply.github.com>
2021-05-18 22:06:00 +0200

    Change height of footer for collection-item

---
+++ client/build/precache-manifest.3b4c32d37d900aa3dd8f48664590f31a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26b2efd18dbd7ec379c227f1a4a38db7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "53462056877aa2864d7d",
+    "url": "/static/css/main.bb6609fa.chunk.css"
+  },
+  {
+    "revision": "c523523d412f6b77773a",
+    "url": "/static/js/2.9eee0e20.chunk.js"
+  },
+  {
+    "revision": "8510524df1062c686369df806a289410",
+    "url": "/static/js/2.9eee0e20.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53462056877aa2864d7d",
+    "url": "/static/js/main.c20cb7dd.chunk.js"
+  },
+  {
+    "revision": "3bb6ad068e02dbe3ad36",
+    "url": "/static/js/runtime-main.26a8a993.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/static/media/shopping-bag.30f07071.svg"
+  }
+]);
